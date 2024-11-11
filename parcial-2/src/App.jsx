1bd47db02a6d2dcb9180e071920b0cfaa5afcc8a
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-11-11 12:28:48 -0300

    se añadió bootstrap, se eliminó la base original de vite para colocar por encima diseños propios, y nada, trabajo iniciado básicamente

---
+++ parcial-2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main className='container'>
+      <div className="card m-4">hola</div>
+      <div className="card"></div>
+    </main>
+    </>
+  )
+}
+
+export default App
