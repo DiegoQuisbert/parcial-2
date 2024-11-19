d87e122fc010fa3d20552fc28078e08d911da477
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-11-19 01:18:39 -0300

    Se hizo una base muy pero muy básica, algunas cosas se tomaron del ejemplo de clase, puro maquetado, ni una sola función ni conectar a la api XD

--- parcial-2/src/App.jsx
+++ parcial-2/src/App.jsx
@@ -1,13 +1,66 @@
 import './App.css'
+import Card from './components/Card';
 
 function App() {
 
   return (
     <>
-    <main className='container'>
-      <div className="card m-4">hola</div>
-      <div className="card"></div>
-    </main>
+      <header>
+        <nav className="navbar navbar-expand-lg py-4">
+          <div className="container-fluid d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <a className="navbar-brand" href="#">MovCrits</a>
+              <ul className="navbar-nav ms-3 d-flex flex-row gap-3">
+                <li className="links nav-item">
+                  <a className="links nav-link" href="#">Películas</a>
+                </li>
+                <li className="links nav-item">
+                  <a className="nav-link" href="#">Directores</a>
+                </li>
+              </ul>
+            </div>
+            <form className="d-flex mx-auto" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success boton" type="submit">Buscar</button>
+            </form>
+            <a className="btn btn-outline-success boton" href="#">Iniciar Sesión</a>
+          </div>
+        </nav>
+
+      </header>
+
+      <main>
+        <div className="row">
+          <div className="col-6 position-relative overflow-hidden">
+            <img
+              src="../public/img/fondo.png"
+              alt="Ejemplo"
+              className="fondo"
+            />
+          </div>
+          <div className="col-6 d-flex flex-column justify-content-center">
+            <h3 className="fw-bold">Hola</h3>
+            <p>¿Te gustaría leer opiniones de verdaderos espectadores en lugar de supuestos críticos o gente sobornada?, MovCrit es tu lugar, En nuestro sitio serás perfectamente capaz de reseñar tus películas favoritas mediante comentarios, ¡libre para todo el mundo!</p>
+            <p>Iniciá sesión ahora y compartinos tu opinión sobre cualquier pelícua</p>
+          </div>
+        </div>
+
+        <hr />
+        
+        <div className='container mt-2'>
+          <h3 className='text-center fw-bold'>Películas</h3>
+          <Card></Card>
+        </div>
+      
+        <div className='container mt-2'>
+          <h3 className='text-center fw-bold'>Directores</h3>
+          <Card></Card>
+        </div>
+
+      </main>
+
+
+
     </>
   )
 }
---
+++ parcial-2/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = (props) => {
+    return (
+        <button 
+        onClick={props.handleClick}    
+        className={'btn btn-'+ props.color} 
+            type="button">
+                {props.text}
+        </button>
+    )
+}
+
+export default Button
